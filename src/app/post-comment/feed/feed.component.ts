4c0906edf824062e5183ec36b319e48da2ad3c3c
MastourEya <98649205+MastourEya@users.noreply.github.com>
2023-05-05 21:59:00 +0100

    updates

--- src/app/post-comment/feed/feed.component.ts
+++ src/app/post-comment/feed/feed.component.ts
@@ -73,27 +73,27 @@ const newComment: Comment = {
 //  }
 
  
-addReply(commentId:number,content: string) {
-const newComment: Comment = {
-  id: Date.now(),
-  showReply:false,
-  content,
-  postId:this.postId,
-  commentId,
-  replies: []
-};
+// addReply(commentId:number,content: string) {
+// const newComment: Comment = {
+//   id: Date.now(),
+//   showReply:false,
+//   content,
+//   postId:this.postId,
+//   commentId,
+//   replies: []
+// };
 
-  this.commentService.addComment(commentId,newComment ).subscribe(
-    (comment) => {
-      const commentIndex = this.comments.findIndex(comment => comment.id === commentId);
-      this.posts[commentIndex].comments.push(comment);
-      console.log('Comment added successfully:', comment);
-    },
-    error => {
-      console.error('Error adding comment:', error);
-    }
-  );
-  }
+//   this.commentService.addComment(commentId,newComment ).subscribe(
+//     (comment) => {
+//       const commentIndex = this.comments.findIndex(comment => comment.id === commentId);
+//       this.posts[commentIndex].comments.push(comment);
+//       console.log('Comment added successfully:', comment);
+//     },
+//     error => {
+//       console.error('Error adding comment:', error);
+//     }
+//   );
+//   }
 
 deleteComment(id: number): void {
   if (confirm("Are you sure you want to delete this comment?")) {
